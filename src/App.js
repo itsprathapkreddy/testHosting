40a188318c5ef7aca4d480c0d3d0792906e5bf9a
Prathap Reddy <kprathapreddy1997@gmail.com>
2022-03-09 12:35:38 -0500

    sdfs

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
 				<p>
-					Edit <code>src/App.js</code> and save to reload.Hai Loading
+					Edit <code>src/App.js</code> and save to reload.Hai Loadingfsf
 				</p>
 				<a
 					className='App-link'
